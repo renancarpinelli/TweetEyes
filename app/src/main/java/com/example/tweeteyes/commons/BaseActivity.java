8368b4bcfa13c0783ddd4bc1db9197252bfa0122
Renan Carpinelli <renan_carpinelli@hotmail.com>
2019-05-14 20:10:10 -0300

    [FIRST COMMIT]  - Android project structure  - base classes

---
+++ app/src/main/java/com/example/tweeteyes/commons/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.example.tweeteyes.commons;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected int getLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tweeteyes/commons/FlowManager.java
@@ -0,0 +1,10 @@
+package com.example.tweeteyes.commons;
+
+import com.example.tweeteyes.R;
+
+public class FlowManager {
+
+    private BaseActivity mActivity;
+    private int mContainer = R.id.fragment;
+
+}
